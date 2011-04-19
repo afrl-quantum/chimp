511a46e96ca2a677cb71de3eddc945a7d06ed9e0
Spencer E. Olson <olsonse@umich.edu>
2011-04-19 17:22:39 -0600

    add (unfinished) unit test for new from= XML attribute guessing

---
+++ src/chimp/interaction/model/detail/test/inelastic_helpers.cpp
@@ -0,0 +1,39 @@
+/*==============================================================================
+ * Public Domain Contributions 2009 United States Government                   *
+ * as represented by the U.S. Air Force Research Laboratory.                   *
+ * Copyright (C) 2006, 2008 Spencer E. Olson                                   *
+ *                                                                             *
+ * This file is part of CHIMP                                                  *
+ *                                                                             *
+ * This program is free software: you can redistribute it and/or modify it     *
+ * under the terms of the GNU Lesser General Public License as published by    *
+ * the Free Software Foundation, either version 3 of the License, or (at your  *
+ * option) any later version.                                                  *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public        *
+ * License for more details.                                                   *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                             *
+ -----------------------------------------------------------------------------*/
+
+
+/** \file
+ * Test file for the inelastic helper classes and routines.
+ * */
+#define BOOST_TEST_MODULE  inelastic_helpers
+
+
+#include <chimp/interaction/model/detail/inelastic_helpers.h>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( inelastic_helpers_tests ); // {
+
+  BOOST_AUTO_TEST_CASE( conservation ) {
+  }
+
+BOOST_AUTO_TEST_SUITE_END(); // }
